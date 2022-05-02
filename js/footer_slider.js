0191c46b7c4ec210113f831bb477edd0aadaf948
hannabananah <hanna0175@gmail.com>
2022-05-02 13:27:47 +0900

    modified footer section

--- js/footer_slider.js
+++ js/footer_slider.js
@@ -1,9 +1,9 @@
 $(function () {
     $(".control_slider").bxSlider({
-        prevText: '<i class="far fa-arrow-alt-circle-left"></i>',
-        nextText: '<i class="far fa-arrow-alt-circle-right"></i>',
-        startText:'<i class="far fa-play-circle"></i>',
-        stopText:'<i class="far fa-pause-circle"></i>',
+        prevText: '<i class="far fa-arrow-alt-circle-left bx-prev"></i>',
+        nextText: '<i class="far fa-arrow-alt-circle-right bx-next"></i>',
+        startText:'<i class="far fa-play-circle bx-start"></i>',
+        stopText:'<i class="far fa-pause-circle bx-stop"></i>',
         auto: true,
         autoControls:true,
         stopAutoOnclick:true,
@@ -13,14 +13,6 @@ $(function () {
         moveSlides: 1,
         slideWidth: 125,
         speed:600,
-    });
-    $(".bx-stop").click(function() {
-        $(".bx-stop").bxSlider({ display: 'none' });
-        $(".bx-start").bxSlider({ display: 'inline-block' });
-    });
-    $(".bx-start").click(function() {
-        $(".bx-stop").bxSlider({ display: 'inline-block' });
-        $(".bx-start").bxSlider({ display: 'none' });
     });
 });
 
